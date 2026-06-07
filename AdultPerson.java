a62fcd395c76793679333c35749fc5bad10bb095
Sherry0521 <chien522956@gmail.com>
2026-06-07 15:00:15 -0700

    AdultPerson.java

--- AdultPerson.java
+++ AdultPerson.java
@@ -2,19 +2,20 @@ import java.awt.*;
 
 public class AdultPerson extends Person {
 
+    // Every time the simulation asks the adult if they want to move, this number goes up by 1.
     private int moveCounter;
-
+    // constructor that creates an adult at a specific location
     public AdultPerson(Location coord) {
         super(coord);
     }
-
+    // constructs an adult who is already sick with a Contagion
     public AdultPerson(Location coord, Contagion disease) {
         super(coord, disease);
     }
 
     @Override
     public boolean willMove() {
-
+        // dead person won't move anymore
         if (!isAlive()) {
             return false;
         }
@@ -27,8 +28,10 @@ public class AdultPerson extends Person {
     public String getType() {
         return "Adult";
     }
+    
     @Override
     public void attemptRecovery() {
+        // If the adult is currently sick (isInfected()), they automatically take 1 point of health damage
         if (isInfected()) {
             damage(1);
         }
@@ -37,23 +40,23 @@ public class AdultPerson extends Person {
 
     @Override
     public Color getColor() {
-
+        // turns gray if the person is dead
         if (!isAlive()) {
             return Color.GRAY;
         }
-
+        // turns red if the person is infected
         if (isInfected()) {
             return Color.RED;
         }
-
+        // turns blue if the person is vaccinated
         if (isVaccinated()) {
             return Color.BLUE;
         }
-
+        // turns cyan if the person is immune
         if (isImmune()) {
             return Color.CYAN;
         }
-
+        // keep its own color if the person is healthy(none of above happened)
         return new Color(34, 180, 34);
     }
 
